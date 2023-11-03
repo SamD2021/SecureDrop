205ff5888e6df05c2cd71236d1dcb1cf81f51824
plabbe98 <148809771+plabbe98@users.noreply.github.com>
2023-11-03 18:03:05 -0400

    userLogin.py
    adding secure drop shell functionality

--- userLogin.py
+++ userLogin.py
@@ -1,59 +1,62 @@
 import os
 import json
+import getpass
 
-#specify path to user information
-path = '/home/student/milestone1/users.txt'
+def main():
+	#specify path to user information
+	path = '/home/student/milestone1/users.txt'
 
-#Check if the path exists
-isExist = os.path.exists(path)
+	#Check if the path exists
+	isExist = os.path.exists(path)
 
-#Open file and check if any users registered
-if isExist == True:
-	o = open('users.txt', "r")
-	toParse = o.read()
-
-	#if file only has 2 charcters it is 'empty' just a placeholder
-	if len(toParse) == 2:
-		print('No users are registered with this client.\n'
-		      'Do you want to register a new user (y/n)?')
-		s = input(' ')
-
-		#if do not want to register a user, quit
-		if s == 'n':
-			quit()
-
-		#elif want to register a user, prompt the different info and take in user input
-		elif s == 'y':
-			print('Enter full name: ')
-			name = input(' ')
-			print('Enter e-mail address: ')
-			email = input(' ')
-			print('Enter password: ')
-			password = input(' ')
-			print('Re-enter password: ')
-			repassword = input(' ')
-
-			#check to see if password and password re-entry match
-			if password == repassword:
-				print('Passwords match.')
-
-			#build user object to be sent to json file
-			user = {
-				'name': name,
-				'email': email,
-				'password': password
-			}
-
-			#send object to json, write to file
-			info = json.dumps(user)
-			f = open('users.txt', "w")
-			f.write(info)
-			f.close
-		#end elif
-
-	#else: users already registered with client
-	file_in = open('users.txt', "r")
-	data = file_in.read()
-	file_in.close()
-	user = json.loads(data)
+	#Open file and check if any users registered
+	if isExist == True:
+		o = open('users.txt', "r")
+		toParse = o.read()
 	
+		#if file only has 2 charcters it is 'empty' just a placeholder
+		if len(toParse) <= 2:
+			print('No users are registered with this client.'
+			      'Do you want to register a new user (y/n)?')
+			s = input(' ')
+	
+	#if do not want to register a user, quit
+			if s == 'n':
+				quit()
+	
+			#elif want to register a user, prompt the different info and take in user input
+			elif s == 'y':
+				print('Enter full name: ')
+				name = input(' ')
+				print('Enter e-mail address: ')
+				email = input(' ')
+				print('Enter password: ')
+				password = getpass.getpass()
+				print('Re-enter password: ')
+				repassword = getpass.getpass()
+	
+				while password != repassword:
+					print('Passwords do not match. Re-enter password.')
+					print('Enter password: ')
+					password = getpass.getpass()
+					print('Re-enter password')
+					repassword = getpass.getpass()
+	
+	
+				#check to see if password and password re-entry match
+				if password == repassword:
+					print('Passwords match.')
+	
+				#build user object to be sent to json file
+				user = {
+					'name': name,
+					'email': email,
+					'password': password
+				}
+	
+				#send object to json, write to file
+				info = json.dumps(user)
+				f = open('users.txt', "w")
+				f.write(info)
+				f.close
+			#end elif
