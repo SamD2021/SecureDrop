c139f81c5edbda80e77844f41bbb694580050c94
markschmidt-ship-it <149639196+markschmidt-ship-it@users.noreply.github.com>
2023-11-12 21:41:20 +0000

    Add files via upload

--- userLogin.py
+++ userLogin.py
@@ -1,59 +1,165 @@
 import os
 import json
+import getpass
+import hashlib 
+from cryptography.fernet import Fernet
+
+def login():
+	print('Enter e-mail address: ')
+	email = input(' ')
+	print('Enter password: ')
+	password = getpass.getpass()
+
+        # Password Salting: Adding salt to the password
+	with open("salt.txt", "r") as salt_file:
+		salt = salt_file.read()
+	#password + salt for added security
+	salted_password = password + salt 
+ 	
+	#reading the key from the key file
+	with open("password_key.bin", "rb") as key_file:
+		key = key_file.read()
+	#reading the encrypted password from password file
+	with open("password_file.bin", "rb") as password_to_match:
+		pass_match = password_to_match.read()
+	
+	#generating the key using the key read in
+	cipher_suite = Fernet(key)
+        # Decrypting password 
+	decrypted_password = cipher_suite.decrypt(pass_match).decode() 
+	
+	#checking if the decrypted password matches the password input with salt
+	#if they do not match keeping doing it an checking
+	while decrypted_password != salted_password:
+		print('Email and password combinations invalid! Re-enter password.')
+		print('Enter e-mail address: ')
+		email = input(' ')
+		print('Enter password: ')
+		password = getpass.getpass()
+		with open("salt.txt", "r") as salt_file:
+			salt = salt_file.read()
+		salted_password = password + salt
+
+		with open("password_key.bin", "rb") as key_file:
+			key = key_file.read()
+		with open("password_file.bin", "rb") as password_to_match:
+			pass_match = password_to_match.read()
+
+		cipher_suite = Fernet(key)
+        	# Decrypting password
+		decrypted_password = cipher_suite.decrypt(pass_match).decode()
+
+	print('Welcome to SecureDrop.')
+	print('Type "help" For Commands.')
+	request = input(' ')
+	while(request != 'exit'):
+		request = input(' secure_drop> ')
+		if request == 'help':
+			print('"add" -> Add a new contact')
+			print('"list" -> List all online contacts')
+			print('"send" -> Transfer file to contact')
+			print('"exit" -> Exit SecureDrop')
+		elif request == 'add':
+			print('Enter full name: ')
+			name = input(' ')
+			print('Enter e-mail address: ')
+			email = input(' ')
+                        #build user object to be sent to json file
+			contact = {
+				'name': name,
+				'email': email
+			}
+                
+                        #send object to json, write to file
+			info = json.dumps(contact)
+			f = open('contacts.txt', "w")
+			f.write(info)
+			f.close
+		elif request == 'list':
+			print('will implement next')
+		elif request == 'send':
+			print('will implement after list')
+	return
+
+def register():
+	print('Enter full name: ')
+	name = input(' ')
+	print('Enter e-mail address: ')
+	email = input(' ')
+	print('Enter password: ')
+	password = getpass.getpass()
+	print('Re-enter password: ')
+	repassword = getpass.getpass()
+
+	while password != repassword:
+		print('Passwords do not match. Re-enter password.')
+		print('Enter password: ')
+		password = getpass.getpass()
+		print('Re-enter password')
+		repassword = getpass.getpass()
+
+
+        #check to see if password and password re-entry match
+	if password == repassword:
+		print('Passwords match.')
+		print('User registered.')
+		print('Exiting SecureDrop.')
+
+        # Password Salting: Adding salt to the password before hashing password using SHA-256
+	salt = "somesalt"
+
+	with open("salt.txt", "w") as salt_file:
+		salt_file.write(salt)
+ 
+	salted_password = hashlib.sha256((password + salt).encode()).hexdigest() 
+
+        # Password Encryption: Encrypting and decrypting password using Fernet symmetric encryption 
+	key = Fernet.generate_key()
+	
+	with open("password_key.key", "wb") as key_file:
+		key_file.write(key)
+ 
+	cipher_suite = Fernet(key) 
+
+        # Encrypting password 
+	encrypted_password = cipher_suite.encrypt(salted_password.encode()).decode()
+
+        #build user object to be sent to json file
+	user = {
+		'name': name,
+		'email': email,
+		'password': encrypted_password
+	}
+
+        #send object to json, write to file
+	info = json.dumps(user)
+	f = open('users.txt', "w")
+	f.write(info)
+	f.close
+	return
 
 #specify path to user information
-path = '/home/student/milestone1/users.txt'
+path = '/home/student/milestone1/users.json'
 
 #Check if the path exists
 isExist = os.path.exists(path)
 
 #Open file and check if any users registered
 if isExist == True:
-	o = open('users.txt', "r")
-	toParse = o.read()
+        o = open('users.json', "r")
+        toParse = o.read()
 
-	#if file only has 2 charcters it is 'empty' just a placeholder
-	if len(toParse) == 2:
-		print('No users are registered with this client.\n'
-		      'Do you want to register a new user (y/n)?')
-		s = input(' ')
+        #if file only has 2 charcters it is 'empty' just a placeholder
+        if len(toParse) <= 2:
+                print('No users are registered with this client.'
+                      'Do you want to register a new user (y/n)?')
+                s = input(' ')
 
-		#if do not want to register a user, quit
-		if s == 'n':
-			quit()
-
-		#elif want to register a user, prompt the different info and take in user input
-		elif s == 'y':
-			print('Enter full name: ')
-			name = input(' ')
-			print('Enter e-mail address: ')
-			email = input(' ')
-			print('Enter password: ')
-			password = input(' ')
-			print('Re-enter password: ')
-			repassword = input(' ')
-
-			#check to see if password and password re-entry match
-			if password == repassword:
-				print('Passwords match.')
-
-			#build user object to be sent to json file
-			user = {
-				'name': name,
-				'email': email,
-				'password': password
-			}
-
-			#send object to json, write to file
-			info = json.dumps(user)
-			f = open('users.txt', "w")
-			f.write(info)
-			f.close
-		#end elif
-
-	#else: users already registered with client
-	file_in = open('users.txt', "r")
-	data = file_in.read()
-	file_in.close()
-	user = json.loads(data)
-	
+                #if do not want to register a user, quit
+                if s == 'n':
+                        quit()
+                #elif want to register a user, prompt the different info and take in user input
+                elif s == 'y':
+                        register()
+                #end elif
+        login()
--- userReg.py
+++ userReg.py
@@ -1,19 +1,22 @@
 import os
 import json
+import getpass
+import hashlib 
+from cryptography.fernet import Fernet
 
 #specify path to user information
-path = '/home/student/milestone1/users.txt'
+path = '/home/student/milestone1/users.json'
 
 #Check if the path exists
 isExist = os.path.exists(path)
 
 #Open file and check if any users registered
 if isExist == True:
-	o = open('users.txt', "r")
+	o = open('users.json', "r")
 	toParse = o.read()
-
+	o.close
 	#if file only has 2 charcters it is 'empty' just a placeholder
-	if len(toParse) == 2:
+	if len(toParse) <= 2:
 		print('No users are registered with this client.'
 		      'Do you want to register a new user (y/n)?')
 		s = input(' ')
@@ -29,24 +32,54 @@ if isExist == True:
 			print('Enter e-mail address: ')
 			email = input(' ')
 			print('Enter password: ')
-			password = input(' ')
+			password = getpass.getpass()
 			print('Re-enter password: ')
-			repassword = input(' ')
+			repassword = getpass.getpass()
+
+			while password != repassword:
+				print('Passwords do not match. Re-enter password.')
+				print('Enter password: ')
+				password = getpass.getpass()
+				print('Re-enter password')
+				repassword = getpass.getpass()
+
 
 			#check to see if password and password re-entry match
 			if password == repassword:
 				print('Passwords match.')
 
+			# Password Salting: Adding salt to the password for added security
+			salt = "somesalt"
+			#writing out the salt to a file for later use
+			with open("salt.txt", "w") as salt_file:
+				salt_file.write(salt)
+ 			#passowrd plus the salt for the password
+			salted_password = password + salt 
+
+			# Password Encryption: Encrypting and decrypting password using Fernet symmetric encryption 
+			key = Fernet.generate_key()
+			#write the password out to a bin file
+			with open("password_key.bin", "wb") as key_file:
+				key_file.write(key)
+			#create a key
+			cipher_suite = Fernet(key) 
+
+			# Encrypting password 
+			encrypted_password = cipher_suite.encrypt(salted_password.encode())
+			#writing the password out to a file that is encrypted for later comparison
+			with open("password_file.bin", "wb") as password_file:
+				password_file.write(encrypted_password)
+
 			#build user object to be sent to json file
 			user = {
 				'name': name,
 				'email': email,
-				'password': password
+				'password': encrypted_password.decode()
 			}
 
 			#send object to json, write to file
 			info = json.dumps(user)
-			f = open('users.txt', "w")
+			f = open('users.json', "w")
 			f.write(info)
 			f.close
 		#end elif
