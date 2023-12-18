e7a40bc0252e6cf4b7cf71245507a5fc81cb060d
SamD2021 <85967506+SamD2021@users.noreply.github.com>
2023-12-18 17:41:31 -0500

    Changed the public key to work with json

--- securedrop.py
+++ securedrop.py
@@ -12,6 +12,8 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 
 
 class SecureDrop:
